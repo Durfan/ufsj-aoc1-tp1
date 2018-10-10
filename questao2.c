b7df0a7ba3fc4015701b5565d03617bf34151cde
Durfan <31577607+Durfan@users.noreply.github.com>
2018-10-10 16:38:41 -0300

    codigo C

--- questao2.c
+++ questao2.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 
-void leArquivo();
-void salvaArquivo();
-void manipulaString();
+void leArquivo(char string[]) {
+    FILE* fp = fopen("entrada.txt", "r");
+    fgets(string,50,fp);
+    fclose(fp);
+}
+
+void manipulaString(char string[]) {
+    int i = 0;
+    while (string[i] != '\0') {
+        if (string[i] < 97) string[i] += 32;
+        else string[i] -= 32;
+        i++;
+    }
+}
+
+void salvaArquivo(char string[50]) {
+    FILE* fp = fopen("saida.txt", "w");
+    fprintf(fp,"%s",string);
+    fclose(fp);
+}
+
 
 int main(void) {
-    char string;
+    char string[50];
+
+    leArquivo(string);
+    manipulaString(string);
+    salvaArquivo(string);
 
     return 0;
 }
