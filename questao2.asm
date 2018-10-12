45da1f07b6aa039931fb7702745138b4737f570e
Durfan <31577607+Durfan@users.noreply.github.com>
2018-10-12 18:05:40 -0300

    questao2 proc

--- questao2.asm
+++ questao2.asm
@@ -4,29 +4,35 @@ fout:    .asciiz "/home/cecilio/git/ufsj-aoc1-tp1/saida.txt"
 string:  .space 1024
 
 .text
+main:
+  jal leArquivo
+  jal manipulaString
+  jal salvaArquivo
+  jal exit
+
 leArquivo:
-    # Open (for reading) a file
-    li   $v0, 13	# system call for open file
-    la   $a0, fin	# input file name
-    li   $a1, 0		# flag for reading
-    li   $a2, 0		# mode is ignored
-    syscall		    # open a file
-    move $s0, $v0	# save the file descriptor
+  # Open (for reading) a file
+  li   $v0, 13        # system call for open file
+  la   $a0, fin       # input file name
+  li   $a1, 0         # flag for reading
+  li   $a2, 0         # mode is ignored
+  syscall             # open a file
+  move $s0, $v0       # save the file descriptor
    
-    # reading from file
-    li   $v0, 14        # system call for reading from file
-    move $a0, $s0       # file descriptor 
-    la   $a1, string    # address of buffer from which to read
-    li   $a2, 255       # hardcoded buffer length
-    syscall             # read from file
+  # reading from file
+  li   $v0, 14        # system call for reading from file
+  move $a0, $s0       # file descriptor 
+  la   $a1, string    # address of buffer from which to read
+  li   $a2, 255       # hardcoded buffer length
+  syscall             # read from file
     
-    # Close the file
-    li   $v0, 16        # system call for close file
-    move $a0, $s0       # file descriptor to close
-    syscall             # close file
+  # Close the file
+  li   $v0, 16        # system call for close file
+  move $a0, $s0       # file descriptor to close
+  syscall             # close file
 
 manipulaString:  
-  la $t0, string            # Load here the string
+  la $t0, string      # Load here the string
   
   Loop:  
     lb   $t2, ($t0)         # We do as always, get the first byte pointed by the address
@@ -69,25 +75,25 @@ manipulaString:
     
 salvaArquivo:
   # Open (for writing) a file that does not exist
-  li   $v0, 13       # system call for open file
-  la   $a0, fout     # output file name
-  li   $a1, 1        # Open for writing (flags are 0: read, 1: write)
-  li   $a2, 0        # mode is ignored
-  syscall            # open a file (file descriptor returned in $v0)
-  move $s6, $v0      # save the file descriptor 
+  li   $v0, 13        # system call for open file
+  la   $a0, fout      # output file name
+  li   $a1, 1         # Open for writing (flags are 0: read, 1: write)
+  li   $a2, 0         # mode is ignored
+  syscall             # open a file (file descriptor returned in $v0)
+  move $s6, $v0       # save the file descriptor 
 
   # Write to file just opened
-  li   $v0, 15       # system call for write to file
-  move $a0, $s6      # file descriptor 
-  la   $a1, string   # address of buffer from which to write
-  add  $a2, $a2, $t3 # hardcoded buffer length
-  syscall            # write to file
+  li   $v0, 15        # system call for write to file
+  move $a0, $s6       # file descriptor 
+  la   $a1, string    # address of buffer from which to write
+  add  $a2, $a2, $t3  # hardcoded buffer length
+  syscall             # write to file
 
   # Close the file 
-  li   $v0, 16       # system call for close file
-  move $a0, $s6      # file descriptor to close
-  syscall            # close file
+  li   $v0, 16        # system call for close file
+  move $a0, $s6       # file descriptor to close
+  syscall             # close file
 
-# Exit != dropped off bottom
-li $v0, 10
-syscall
+exit:
+  li $v0, 10
+  syscall
