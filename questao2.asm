25b6372e13f84b574f4a27f39c67c96302a7c8f7
Durfan <31577607+Durfan@users.noreply.github.com>
2018-10-13 23:10:49 -0300

    doc OCD

--- questao2.asm
+++ questao2.asm
@@ -31,10 +31,10 @@ leArquivo:            # procedimento para ler o arquivo
   li   $v0, 16        # system call: Fecha o arquivo
   syscall             # Feche o arquivo!
   
-  jr $ra              # Retorna do procediemnto
+  jr $ra              # Retorna do procedimento
 
 manipulaString:             # procedimento para alterar a string
-  Loop:                     # loop ṕara cada character da string
+  Loop:                     # loop para cada character da string
     lb   $t2, ($a0)         # Aponta $t2 para a posicao de endereco de $a0
     beq  $t2, $zero, End    # Se $t2 conter NULL, a string terminou, End
     slti $t1, $t2, 90       # $t2 e menor que 90(Z)?
@@ -50,7 +50,7 @@ manipulaString:             # procedimento para alterar a string
     sb   $t2, ($a0)         # armazena o valor na posicao em $a0
     j Next                  # Va pra Next
     
-    Lower:                  # rotina para lowercase 
+    Lower:                  # rotina para lowercase
     slti $t1, $t2, 97       # $t2 e menor que 97(a)?
     bne  $t1, $zero, Next   # Se $t1 for 1 entao nao e uma letra, Next
     addi $t2, $t2, -32      # Soma -32 a $t2
@@ -60,25 +60,25 @@ manipulaString:             # procedimento para alterar a string
     addi $a0, $a0, 1        # Incrementa o endereco de $a0
     j Loop                  # Va para Loop
 
-  End:                      # Termino do Loop  
+  End:                      # Termino do Loop
     #li $v0, 4               # system call: Imprimir string
     #la $a0, string          # endereco da string
     #syscall                 # Imprima!
-    jr $ra                  # Retorna do procediemnto
+    jr $ra                  # Retorna do procedimento
     
 salvaArquivo:         # procedimento para salvar o arquivo
   li   $v0, 13        # system call: Abre o arquivo
   li   $a1, 1         # flag para escrita
   li   $a2, 0         # modo ignorado
   syscall             # Abra o arquivo!
-  move $t0, $v0       # Salva o descriptor em $t0 
+  move $t0, $v0       # Salva o descriptor em $t0
 
   li   $v0, 15        # system call: Escrevendo no arquivo
-  move $a0, $t0       # Carrega o descriptor do arquivo 
+  move $a0, $t0       # Carrega o descriptor do arquivo
   la   $a1, string    # endereço do buffer
   move $a2, $s0       # carrega strlen de $s0
   syscall             # Escreva no arquivo!
 
   li   $v0, 16        # system call: Fecha o arquivo
   syscall             # Feche o arquivo!
-  jr $ra              # Retorna do procediemnto
+  jr $ra              # Retorna do procedimento
